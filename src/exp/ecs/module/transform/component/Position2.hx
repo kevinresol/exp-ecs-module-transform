b75a5e2fae9159a76b64623cb28eccf6dd649bb1
Kevin Leung <kevinresol@gmail.com>
2020-10-25 14:45:30 +0800

    helper methods

--- src/exp/ecs/module/transform/component/Position2.hx
+++ src/exp/ecs/module/transform/component/Position2.hx
@@ -3,4 +3,13 @@ package exp.ecs.module.transform.component;
 class Position2 implements Component {
 	public var x:Float;
 	public var y:Float;
+
+	public inline function set(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public inline function copyFrom(other:Position2) {
+		set(other.x, other.y);
+	}
 }
